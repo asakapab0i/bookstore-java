0a992efac10210f130919f5754fd915ae43c2f8a
Bryan Bojorque <bryan@di.net.au>
2019-06-08 01:45:06 +0800

    v1 - war packaging
    Signed-off-by: Bryan Bojorque <bryan@di.net.au>

--- src/main/java/com/bryan/bookstore/App.java
+++ src/main/java/com/bryan/bookstore/App.java
@@ -2,12 +2,13 @@ package com.bryan.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class App {
+public class App extends SpringBootServletInitializer {
     public static void main( String[] args ){
         SpringApplication.run(App.class, args);
     }
